835f6c8dbc69b57061b3e3d418e171da44d97c20
Ze-Hao Wang (Salmon) <salmon.zh.tw@gmail.com>
2020-10-07 21:13:30 +0800

    update translations

--- tools/translate-gen-master-json.ts
+++ tools/translate-gen-master-json.ts
@@ -4,10 +4,12 @@ import _ from 'lodash';
 import appRoot from 'app-root-path';
 // Local modules.
 import SkillData from '../src/data/ja-jp/SkillMaster.json';
+import StatusData from '../src/data/ja-jp/StatusMaster.json';
 
 const main = async () => {
-  const rawData = fs.readFileSync(`${appRoot}/translations/skills_zh-tw.tsv`, 'utf-8');
-  const updatedSkills = rawData.split(/\n/).map((line) => {
+  // File 1.
+  const skillsRawData = fs.readFileSync(`${appRoot}/translations/skills_zh-tw.tsv`, 'utf-8');
+  const updatedSkills = skillsRawData.split(/\n/).map((line) => {
     const [skillId, skillName, description] = line.split('\t');
     return { skillId, skillName, description };
   });
@@ -19,9 +21,27 @@ const main = async () => {
     });
   });
 
-  const text = JSON.stringify(SkillData, null, 2) + '\n';
+  const skillText = JSON.stringify(SkillData, null, 2) + '\n';
 
-  fs.writeFileSync(`${appRoot}/src/data/zh-tw/SkillMaster.json`, text, 'utf-8');
+  fs.writeFileSync(`${appRoot}/src/data/zh-tw/SkillMaster.json`, skillText, 'utf-8');
+
+  // File 2.
+  const statusRawData = fs.readFileSync(`${appRoot}/translations/status_zh-tw.tsv`, 'utf-8');
+  const updatedStatuses = statusRawData.split(/\n/).map((line) => {
+    const [statusId, statusName, description] = line.split('\t');
+    return { statusId, statusName, description };
+  });
+
+  updatedStatuses.forEach((status) => {
+    Object.assign(_.get(StatusData, status.statusId), {
+      ...status,
+      statusId: Number(status.statusId),
+    });
+  });
+
+  const statusText = JSON.stringify(StatusData, null, 2) + '\n';
+
+  fs.writeFileSync(`${appRoot}/src/data/zh-tw/StatusMaster.json`, statusText, 'utf-8');
 };
 
 main();
